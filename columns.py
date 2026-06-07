3f084867f03fa609344ffcf81a850c856f281fa6
MBlesx <blesmaddy@gmail.com>
2026-06-07 17:47:33 +0200

    Add files via upload

---
+++ columns.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun  5 15:06:12 2026
+
+@author: maddyxbles
+"""
+
+# listst of columns 
+
+# Lists of different columns 
+# Appropriation + credibility answers 
+app_cred = ["Neutral Non-Severe_1",
+"Neutral Non-Severe_2",
+"Neutral Non-Severe_3",
+"Neutral Non-Severe_4",
+"Neutral Non-Severe_5",
+"Neutral Non-Severe_6",
+"Neutral Non-Severe_7",
+"Neutral Non-Severe_8",
+"Happy Non-Severe_1",
+"Happy Non-Severe_2",
+"Happy Non-Severe_3",
+"Happy Non-Severe_4",
+"Happy Non-Severe_5",
+"Happy Non-Severe_6",
+"Happy Non-Severe_7",
+"Happy Non-Severe_8",
+"Ex. Happy Non-Severe_1",
+"Ex. Happy Non-Severe_2",
+"Ex. Happy Non-Severe_3",
+"Ex. Happy Non-Severe_4", 
+"Ex. Happy Non-Severe_5",
+"Ex. Happy Non-Severe_6",
+"Ex. Happy Non-Severe_7",
+"Ex. Happy Non-Severe_8",
+"Neutral Severe_1",
+"Neutral Severe_2",
+"Neutral Severe_3",
+"Neutral Severe_4", 
+"Neutral Severe_5",
+"Neutral Severe_6",
+"Neutral Severe_7",
+"Neutral Severe_8",
+"Happy Severe Neutral_1", 
+"Happy Severe Neutral_2", 
+"Happy Severe Neutral_3", 
+"Happy Severe Neutral_4", 
+"Happy Severe Neutral_5", 
+"Happy Severe Neutral_6", 
+"Happy Severe Neutral_7", 
+"Happy Severe Neutral_8", 
+"Ex. Happy Severe_1",
+"Ex. Happy Severe_2",
+"Ex. Happy Severe_3",
+"Ex. Happy Severe_4",
+"Ex. Happy Severe_5",
+"Ex. Happy Severe_6",
+"Ex. Happy Severe_7",
+"Ex. Happy Severe_8"]
+
+#---------------
+
+# emotion and severity separated
+neutral_nons = ["Neutral Non-Severe_1",
+"Neutral Non-Severe_2",
+"Neutral Non-Severe_3",
+"Neutral Non-Severe_4",
+"Neutral Non-Severe_5",
+"Neutral Non-Severe_6",
+"Neutral Non-Severe_7",
+"Neutral Non-Severe_8"]
+
+happy_nons = ["Happy Non-Severe_1",
+"Happy Non-Severe_2",
+"Happy Non-Severe_3",
+"Happy Non-Severe_4",
+"Happy Non-Severe_5",
+"Happy Non-Severe_6",
+"Happy Non-Severe_7",
+"Happy Non-Severe_8"]
+
+exhappy_nons = ["Ex. Happy Non-Severe_1",
+"Ex. Happy Non-Severe_2",
+"Ex. Happy Non-Severe_3",
+"Ex. Happy Non-Severe_4", 
+"Ex. Happy Non-Severe_5",
+"Ex. Happy Non-Severe_6",
+"Ex. Happy Non-Severe_7",
+"Ex. Happy Non-Severe_8"]
+
+neutral_s = ["Neutral Severe_1",
+"Neutral Severe_2",
+"Neutral Severe_3",
+"Neutral Severe_4", 
+"Neutral Severe_5",
+"Neutral Severe_6",
+"Neutral Severe_7",
+"Neutral Severe_8"]
+
+happy_s = ["Happy Severe Neutral_1", 
+"Happy Severe Neutral_2", 
+"Happy Severe Neutral_3", 
+"Happy Severe Neutral_4", 
+"Happy Severe Neutral_5", 
+"Happy Severe Neutral_6", 
+"Happy Severe Neutral_7", 
+"Happy Severe Neutral_8"]
+
+exhappy_s = ["Ex. Happy Severe_1",
+"Ex. Happy Severe_2",
+"Ex. Happy Severe_3",
+"Ex. Happy Severe_4",
+"Ex. Happy Severe_5",
+"Ex. Happy Severe_6",
+"Ex. Happy Severe_7",
+"Ex. Happy Severe_8"]
+
+
+#-----------
+
+# appropriation and credibility separated per emotion and severity
+
+neutral_nons_app = ["Neutral Non-Severe_1",
+"Neutral Non-Severe_2",
+"Neutral Non-Severe_3",
+"Neutral Non-Severe_4"]
+
+
+neutral_nons_cred = ["Neutral Non-Severe_5",
+"Neutral Non-Severe_6",
+"Neutral Non-Severe_7",
+"Neutral Non-Severe_8"]
+
+happy_nons_app = ["Happy Non-Severe_1",
+"Happy Non-Severe_2",
+"Happy Non-Severe_3",
+"Happy Non-Severe_4"]
+
+happy_nons_cred = ["Happy Non-Severe_5",
+"Happy Non-Severe_6",
+"Happy Non-Severe_7",
+"Happy Non-Severe_8"]
+
+exhappy_nons_app = ["Ex. Happy Non-Severe_1",
+"Ex. Happy Non-Severe_2",
+"Ex. Happy Non-Severe_3",
+"Ex. Happy Non-Severe_4"]
+ 
+exhappy_nons_cred = ["Ex. Happy Non-Severe_5",
+"Ex. Happy Non-Severe_6",
+"Ex. Happy Non-Severe_7",
+"Ex. Happy Non-Severe_8"]
+
+neutral_s_app = ["Neutral Severe_1",
+"Neutral Severe_2",
+"Neutral Severe_3",
+"Neutral Severe_4"]
+ 
+neutral_s_cred = ["Neutral Severe_5",
+"Neutral Severe_6",
+"Neutral Severe_7",
+"Neutral Severe_8"]
+
+happy_s_app = ["Happy Severe Neutral_1", 
+"Happy Severe Neutral_2", 
+"Happy Severe Neutral_3", 
+"Happy Severe Neutral_4"] 
+
+happy_s_cred = ["Happy Severe Neutral_5", 
+"Happy Severe Neutral_6", 
+"Happy Severe Neutral_7", 
+"Happy Severe Neutral_8"]
+
+exhappy_s_app = ["Ex. Happy Severe_1",
+"Ex. Happy Severe_2",
+"Ex. Happy Severe_3",
+"Ex. Happy Severe_4"]
+
+exhappy_s_cred = ["Ex. Happy Severe_5",
+"Ex. Happy Severe_6",
+"Ex. Happy Severe_7",
+"Ex. Happy Severe_8"]
+
+
+# list of demographic questions answers
+
+demographic = ["Age", 
+               "Gender", 
+               "Level Education", 
+               "Comf. Tech_1", 
+               "Used Assistant",
+               "Familiarity_1"]
+
+
+
+
+
+
+
